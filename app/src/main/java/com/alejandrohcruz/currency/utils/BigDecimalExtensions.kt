4907af0239487d9ee335a301fca2fac4e785f307
Alejandro H. Cruz <alejandrohcruz@outlook.com>
2020-05-24 22:22:37 +0200

    ♻️Refactored the way the numbers are converted to string

--- app/src/main/java/com/alejandrohcruz/currency/utils/BigDecimalExtensions.kt
+++ app/src/main/java/com/alejandrohcruz/currency/utils/BigDecimalExtensions.kt
@@ -1,21 +1,14 @@
 package com.alejandrohcruz.currency.utils
 
 import java.math.BigDecimal
+import java.math.RoundingMode
 
 /**
  * @brief Trims out the decimal values for natural numbers and 00 for decimal numbers
  */
 fun BigDecimal.toPresentableString(): String {
-    this.toPlainString().apply {
-        return if (endsWithDotAndZeros()) {
-            // Round
-            this@toPresentableString.toInt().toBigDecimal().toPlainString()
-        } else if (containsADot() && endsWith("00")) {
-            removeSuffix("00")
-        } else if (containsADot() && endsWithUnnecessaryDecimalZero()) {
-            removeSuffix("0")
-        } else {
-            this
-        }
-    }
+    return this.setScale(2, RoundingMode.HALF_UP) // round
+        .toPlainString()
+        .removeSuffix(".00")
+        .removeSuffix(".0")
 }
